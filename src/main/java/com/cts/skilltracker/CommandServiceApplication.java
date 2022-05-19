2f44c6e0fc82db41744df8050e7d6f6cac31746c
cogjava404 <cogjava404@ctsjava080.iiht.tech>
2022-05-19 20:45:52 +0530

    Added comments
    Change-Id: I3546a5717cb1ecbcfcec088739fc3beb4f9ca899

--- src/main/java/com/cts/skilltracker/CommandServiceApplication.java
+++ src/main/java/com/cts/skilltracker/CommandServiceApplication.java
@@ -3,6 +3,12 @@ package com.cts.skilltracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Command side service
+ * @author cogjava404
+ *
+ */
+
 @SpringBootApplication
 public class CommandServiceApplication {
 
